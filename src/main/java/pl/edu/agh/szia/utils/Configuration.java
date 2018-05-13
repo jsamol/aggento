4b688b3e9315efa0000cb2b9ebdfef735106e760
jsamol <julia.samol@gmail.com>
2018-05-13 17:43:52 +0200

    Add Configuration util class

---
+++ src/main/java/pl/edu/agh/szia/utils/Configuration.java
@@ -0,0 +1,16 @@
+package pl.edu.agh.szia.utils;
+
+public class Configuration {
+    public static final String PLATFORM_IP = "192.168.0.34";
+    public static final int PLATFORM_PORT = 1099;
+    public static final String SERVER_AGENT_NAME = "serverAgent";
+    public static final String CLIENT_AGENT_NAME_PATTERN = "client-%s";
+
+    public static String getServerAgentGuid() {
+        return String.format("%s@%s:%d/JADE", SERVER_AGENT_NAME, PLATFORM_IP, PLATFORM_PORT);
+    }
+
+    public static String getClientAgentName(String username) {
+        return String.format(CLIENT_AGENT_NAME_PATTERN, username);
+    }
+}
